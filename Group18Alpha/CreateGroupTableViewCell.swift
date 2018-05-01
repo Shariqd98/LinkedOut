6d83c8d804a7a4302866b84b80bd5aee2142793c
LZip96 <loganzipkes@utexas.edu>
2018-05-01 00:05:44 -0500

    Added initial foundation for messaging feature

---
+++ Group18Alpha/CreateGroupTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CreateGroupTableViewCell.swift
+//  Group18Alpha
+//
+//  Created by Logan Zipkes on 4/30/18.
+//  Copyright © 2018 CS329E. All rights reserved.
+//
+
+import UIKit
+
+class CreateGroupTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
